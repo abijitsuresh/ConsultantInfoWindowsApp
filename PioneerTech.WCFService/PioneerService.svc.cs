6c019f600e0745fe0d11b9e710449f436e6d6696
Abijit Suresh Geetha Ram <abijit.suresh@outlook.com>
2017-08-23 17:39:16 -0500

    Added WCF service for login. The windows app consumes the service through service reference.

---
+++ PioneerTech.WCFService/PioneerService.svc.cs
@@ -0,0 +1,45 @@
+﻿using PioneerTechSystem.DAL;
+using PioneerTechSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PioneerTech.WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PioneerService : IPioneerService
+    {
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+        
+
+        public int loginCheck(Login loginObj)
+        {            
+            EmployeeDataAccessLayer EmployeeDataAccessLayerObj = new EmployeeDataAccessLayer();
+            int returnValue;
+            if (EmployeeDataAccessLayerObj.loginCheck(loginObj) == 1)
+            {
+                returnValue = 1;
+            }
+            else
+                returnValue = 0;
+            return returnValue;
+        }
+    }
+}
--- PioneerTechConsultancySystem/LoginScreen.cs
+++ PioneerTechConsultancySystem/LoginScreen.cs
@@ -27,13 +27,9 @@ namespace PioneerTechConsultancySystem
             Login LoginObj = new Login();
             LoginObj.LoginID = LoginIdTextBox.Text;
             LoginObj.Password = PasswordTextBox.Text;
-
-            string loginId = LoginIdTextBox.Text;
-            string password = PasswordTextBox.Text;
-
-            EmployeeDataAccessLayer EmployeeDataAccessLayerObj = new EmployeeDataAccessLayer();
-
-            if (EmployeeDataAccessLayerObj.loginCheck(LoginObj) == 1)
+            
+            PioneerServiceReference.PioneerServiceClient serviceClient = new PioneerServiceReference.PioneerServiceClient();
+            if (serviceClient.loginCheck(LoginObj) == 1)
             {
                 MessageBox.Show("Login Success");
                 this.Hide();
